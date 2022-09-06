2d0a098396024abf32fb1d64a9b3ee71529bd08c
Zainab Lawal <zainablaw1012@gmail.com>
2022-09-07 01:19:54 +0300

    Defined a route to get all todos

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express');
+//Setting up the express app
+import express from 'express';
 const app = express();
-const bodyParser = require('body-parser');
+
+//importing the database
+import db from "./db/todos.js";
 
 
 const port = 8000;
 const hostname = 'localhost';
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to the home page")
+    res.send("Welcome to the todo api")
+})
+
+app.get('/api/v1/todos',(req,res)=>{
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        todos: db
+    })
 })
 
 app.listen(port, hostname,()=>{
