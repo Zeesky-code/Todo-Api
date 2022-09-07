cb1476a499cd52e47c4b569524719699b927353b
Zainab Lawal <zainablaw1012@gmail.com>
2022-09-07 03:53:41 +0300

    Added the create todo route

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 import express from 'express';
 const app = express();
 
+import bodyParser from 'body-parser';
+
 //importing the database
 import db from "./db/todos.js";
 
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 8000;
 const hostname = 'localhost';
@@ -13,13 +18,46 @@ app.get('/', (req,res)=>{
     res.send("Welcome to the todo api")
 })
 
+//getting all todos
 app.get('/api/v1/todos',(req,res)=>{
     res.status(200).send({
         success: 'true',
         message: 'todos retrieved successfully',
         todos: db
+    });
+})
+
+//creating a todo
+
+app.post('/api/v1/todos',(req,res)=>{
+    if (!req.body.title){
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    }else if(!req.body.description){
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    }
+    const todo={
+        id: db.length +1,
+        title: req.body.title,
+        description: req.body.description
+    }
+
+    db.push(todo)
+    console.log(db)
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
     })
 })
+
+
+
 
 app.listen(port, hostname,()=>{
     console.log(`Server is running at ${port}`)
